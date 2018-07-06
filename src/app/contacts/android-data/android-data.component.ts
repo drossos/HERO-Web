15855260c22684ac5c2a092f70a50000d3cae12e
Daniel Rossos <d.rossos.53@gmail.com>
2018-07-06 11:09:27 -0400

    Changed names

--- src/app/contacts/android-data/android-data.component.ts
+++ src/app/contacts/android-data/android-data.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
+
 
 @Component({
   selector: 'android-data',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AndroidDataComponent implements OnInit {
 
-  constructor() { }
+  contacts: Contact[]
+  selectedContact: Contact
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+  	this.contactService
+      .getContacts()
+      .then((contacts: Contact[]) => {
+        this.contacts = contacts.map((contact) => {
+          if (!contact.phone) {
+            contact.phone = {
+              mobile: '',
+              work: ''
+            }
+          }
+          return contact;
+        });
+      });
   }
 
 }
