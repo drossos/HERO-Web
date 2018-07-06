f4350e346741dc9c5edc1fb06ee75cf6895108d5
Daniel Rossos <d.rossos.53@gmail.com>
2018-07-06 11:17:09 -0400

    Changed names

--- src/app/contacts/android-data/android-data.component.ts
+++ src/app/contacts/android-data/android-data.component.ts
@@ -1,35 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from '../contact';
-import { ContactService } from '../contact.service';
-import { ContactDetailsComponent } from '../contact-details/contact-details.component';
-
 
 @Component({
-  selector: 'android-data',
+  selector: 'app-android-data',
   templateUrl: './android-data.component.html',
   styleUrls: ['./android-data.component.css']
 })
 export class AndroidDataComponent implements OnInit {
 
-  contacts: Contact[]
-  selectedContact: Contact
-
-  constructor(private contactService: ContactService) { }
+  constructor() { }
 
   ngOnInit() {
-  	this.contactService
-      .getContacts()
-      .then((contacts: Contact[]) => {
-        this.contacts = contacts.map((contact) => {
-          if (!contact.phone) {
-            contact.phone = {
-              mobile: '',
-              work: ''
-            }
-          }
-          return contact;
-        });
-      });
+  	console.log("Within android-data init")
   }
 
 }
