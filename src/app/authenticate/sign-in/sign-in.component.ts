b41ff079bdc9348ca6de5d8ed914677ce90f2517
Daniel Rossos <d.rossos.53@gmail.com>
2018-07-12 15:28:17 -0400

    Testing Therapist db

--- src/app/authenticate/sign-in/sign-in.component.ts
+++ src/app/authenticate/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 //import { AuthenticationService, TokenPayload } from '../authenticate.service';
 import { Router } from '@angular/router';
 import {NgForm} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 /*import {AppComponent} from '../../app.component'*/
 import { ContactListComponent} from '../../contacts/contact-list/contact-list.component';
 import {Therapist} from '../therapist';
