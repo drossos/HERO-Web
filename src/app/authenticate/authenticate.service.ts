ad238b2b67988d4ebb027fa71faca1a1646b2d1a
Daniel Rossos <d.rossos.53@gmail.com>
2018-07-12 10:55:12 -0400

    Added test sign in screen

--- src/app/authenticate/authenticate.service.ts
+++ src/app/authenticate/authenticate.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import {Therapist} from './therapist';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthenticateService {
+	private therapistsUrl = '/api/therapists';
 
-  constructor() { }
 
+  constructor(private http: Http) { }
+	
+	// get("/api/contacts")
+    getTherapists(): Promise<Therapist[]> {
+    	console.log("In auth service");
+      return this.http.get(this.therapistsUrl)
+                 .toPromise()
+                 .then(response => response.json() as Therapist[])
+                 .catch(this.handleError);
+    }
+
+    private handleError (error: any): Promise<any> {
+      let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg); // log to console
+      return Promise.reject(errMsg);
+    }
 }
--- src/app/authenticate/sign-in/sign-in.component.ts
+++ src/app/authenticate/sign-in/sign-in.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 //import { AuthenticationService, TokenPayload } from '../authenticate.service';
 import { Router } from '@angular/router';
 /*import {AppComponent} from '../../app.component'*/
 import { ContactListComponent} from '../../contacts/contact-list/contact-list.component';
-
+import {Therapist} from '../therapist';
+import {AuthenticateService} from '../authenticate.service';
 //TODO IMPLIMENT A USER DATABASE TO ALLOW FOR ONLY SOME PEOPLE TO BE ALLOWED IN
 
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
+  providers: [AuthenticateService]
 })
 export class SignInComponent implements OnInit {
+
+  
+  @Input()
+  therapist :Therapist;
+
   signIn : boolean = true;
 	contactList : ContactListComponent;
  	/*credentials: TokenPayload = {
@@ -19,13 +26,26 @@ export class SignInComponent implements OnInit {
     password: ''
 	};*/
 
-  constructor() { }
+  constructor(private authService:AuthenticateService) { }
 
   ngOnInit() {
   }
 
   login(){
-    this.signIn = false;
+    var dbArr = this.authService.getTherapists();
+
+    if (this.fetchTherapist(dbArr))
+      this.signIn = false;
+    else 
+      console.log("non-valid therapist");
+  }
+
+  fetchTherapist(dbArr){
+    for (var i=0; i < dbArr.length; i++){
+       if ((dbArr[i] as any).name === (this.therapist as any).name && (dbArr[i] as any).hash === (this.therapist as any).hash)
+         return true;
+    }
+    return false;
   }
 
 }
